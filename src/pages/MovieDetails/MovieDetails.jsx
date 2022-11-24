3a4352ed275f6dfbcea8d3e61c295fbb7077e630
Denis Kyzym <102521716+kyzym@users.noreply.github.com>
2022-11-24 18:17:19 +0100

    separated components

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -2,15 +2,13 @@ import { Loader } from 'components/Loader/Loader';
 import { useGetMovieDetails } from 'hooks/useGetMovieDetails';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-import { startImageUrl } from 'utils/api/api';
 import { Box } from 'utils/Box.styled';
 import * as SC from '../MovieDetails/MovieDetails.styled';
+import MovieDetailList from '../../components/MovieDetailsList/MovieDetailList';
+import AdditionalInformationList from 'components/AdditionalInformationList/AdditionalInformationList';
 
 const MovieDetails = () => {
   const { movieDetails, backLinkHref, loading, success } = useGetMovieDetails();
-
-  const { id, title, poster_path, overview, genres, vote_average } =
-    movieDetails;
 
   return (
     <Box as="div" p="20px" maxWidth="1200px" m="0 auto">
@@ -18,45 +16,9 @@ const MovieDetails = () => {
 
       <SC.BackBtn to={backLinkHref}>Back</SC.BackBtn>
 
-      {success && (
-        <SC.MovieDetailList key={id}>
-          <img
-            src={startImageUrl + poster_path}
-            alt="title"
-            width="350px"
-          ></img>
-          <div>
-            <SC.Title>{title}</SC.Title>
-            <SC.SubTitle>Overview:</SC.SubTitle>
-            <SC.TextContent>{overview}</SC.TextContent>
+      {success && <MovieDetailList movieDetails={movieDetails} />}
 
-            <SC.SubTitle>Genres:</SC.SubTitle>
-            <SC.TextContent>
-              {genres.map(({ name }) => name).join(', ')}
-            </SC.TextContent>
-
-            <SC.SubTitle>
-              Users score:
-              <SC.Rating rating={vote_average.toFixed()}>
-                {(vote_average * 10).toFixed(2)}
-              </SC.Rating>
-              %
-            </SC.SubTitle>
-          </div>
-        </SC.MovieDetailList>
-      )}
-
-      <SC.LinkWrapper>
-        <SC.SubTitle>Additional information:</SC.SubTitle>
-        <Box as="ul" display="flex" flexDirection="column" gridGap={20}>
-          <li>
-            <SC.DetailsLink to="cast">Cast</SC.DetailsLink>
-          </li>
-          <li>
-            <SC.DetailsLink to="reviews">Reviews</SC.DetailsLink>
-          </li>
-        </Box>
-      </SC.LinkWrapper>
+      <AdditionalInformationList />
       <Suspense fallback={<Loader />}>
         <Outlet />
       </Suspense>
